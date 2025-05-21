fbfc4589579258aac98dee047ac2a8ed5deb1afc
wileyliao <wileyliao9552@gmail.com>
2025-05-21 11:28:01 +0800

    20250521 restructure for bag from main to sub_rules

--- rx_checker_main.py
+++ rx_checker_main.py
@@ -6,24 +6,13 @@ def main(rx, rule_state):
     messages = []
     error_rule = []
 
-    for bag in rx.get("Data", {}).get("eff_order", []):
-        for order in bag.get("order", []):
+    messages, error_type, error_rule = oral_drug_checker(rx, messages, error_type, error_rule)
+    messages, error_type, error_rule = insured_cannot_selfpay(rx, messages, error_type, error_rule)
+    messages, error_type, error_rule = foreign_control_drug_limit(rx, messages, error_type, error_rule)
+    messages, error_type, error_rule = foreign_max_days(rx, messages, error_type, error_rule)
+    messages, error_type, error_rule = foreign_max_dosage(rx, messages, error_type, error_rule)
+    messages, error_type, error_rule = doctor_cannot_prescribe_self(rx, messages, error_type, error_rule)
 
-            # OMIF審查區間
-            if order.get("CODE", "") == "OMIF":
-                messages, error_type = omif_checker(order, messages, error_type, )
-
-                # 口服藥品審查區間
-            messages, error_type, error_rule = oral_drug_checker(order, messages, error_type, error_rule)
-
-            # 管制藥品審查區間
-            if "內用" not in order.get("TYPE", "") or order.get("DRUGKIND") == "N":
-                return messages, error_type, error_rule
-            messages, error_type, error_rule = insured_cannot_selfpay(order, bag, messages, error_type, error_rule)
-            messages, error_type, error_rule = foreign_control_drug_limit(order, bag, messages, error_type, error_rule)
-            messages, error_type, error_rule = foreign_max_days(order, bag, messages, error_type, error_rule)
-            messages, error_type, error_rule = foreign_max_dosage(order, bag, messages, error_type, error_rule)
-            messages, error_type, error_rule = doctor_cannot_prescribe_self(order, bag, messages, error_type, error_rule)
-
+    messages, error_type, error_rule = omif_checker(rx, messages, error_type, error_rule)
 
     return messages, error_type, error_rule
--- sub_tph_omif_checker.py
+++ sub_tph_omif_checker.py
@@ -1,4 +1,4 @@
-def omif_checker(order, messages, error_type):
+def omif_checker(rx, messages, error_type, error_rule):
     """
         藥品Lorapseudo SR 120mg/5mg(藥品碼OMIF)規定：
             1. 自費
@@ -7,40 +7,50 @@ def omif_checker(order, messages, error_type):
                 --> 1天最多2粒。
 
     """
-    sd = float(order.get("SD", 0))
-    days = int(order.get("DAYS", 0))
-    qty = int(order.get("TXN_QTY", 0))
-    ctype = order.get("CTYPE", "")
+    rule = 'Other-1'
+
+    for bag in rx.get("Data", {}).get("eff_order", []):
+        for order in bag.get("order", []):
+
+            # OMIF審查區間
+            if order.get("CODE", "") == "OMIF":
+                sd = float(order.get("SD", 0))
+                days = int(order.get("DAYS", 0))
+                qty = int(order.get("TXN_QTY", 0))
+                ctype = order.get("CTYPE", "")
 
 
-    freq_times = (qty/days)/sd
-    daily_dose = sd * freq_times
 
-    if ctype == "自費":
-        if daily_dose > 2:
-            messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
-                            f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
-                            f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時每日服用劑量超過上限 (每日最多 2 顆)，"
-                            f"電聯醫師修改。")
-            error_type.append("C劑量錯誤")
-        if days > 30:
-            messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
-                            f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
-                            f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時給藥天數超過 30 日限制，"
-                            f"電聯醫師修改。")
-            error_type.append("F數量錯誤")
-        if qty > 60:
-            messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
-                            f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
-                            f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時總顆數超過 60 粒限制，"
-                            f"電聯醫師修改。")
-            error_type.append("F數量錯誤")
-    elif ctype == "健保":
-        if daily_dose > 2:
-            messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
-                            f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
-                            f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為健保時每日服用劑量超過上限 (每日最多 2 顆)，"
-                            f"電聯醫師修改。")
-            error_type.append("C劑量錯誤")
 
-    return messages, error_type
+                freq_times = (qty/days)/sd
+                daily_dose = sd * freq_times
+
+                if ctype == "自費":
+                    if daily_dose > 2:
+                        messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
+                                        f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
+                                        f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時每日服用劑量超過上限 (每日最多 2 顆)，"
+                                        f"電聯醫師修改。")
+                        error_type.append("C劑量錯誤")
+                    if days > 30:
+                        messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
+                                        f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
+                                        f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時給藥天數超過 30 日限制，"
+                                        f"電聯醫師修改。")
+                        error_type.append("F數量錯誤")
+                    if qty > 60:
+                        messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
+                                        f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
+                                        f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為自費時總顆數超過 60 粒限制，"
+                                        f"電聯醫師修改。")
+                        error_type.append("F數量錯誤")
+                elif ctype == "健保":
+                    if daily_dose > 2:
+                        messages.append(f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
+                                        f"每次{order.get('SD', '')}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
+                                        f"依據相關規定，{order.get('DIANAME') or order.get('NAME')} 為健保時每日服用劑量超過上限 (每日最多 2 顆)，"
+                                        f"電聯醫師修改。")
+                        error_type.append("C劑量錯誤")
+                        error_rule.append(rule)
+
+    return messages, error_type, error_rule
