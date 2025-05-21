b182fe23f458689ba94f309d53203a586a770a79
wileyliao <wileyliao9552@gmail.com>
2025-05-21 19:54:31 +0800

    20250521 rule out OMIF in these checker

--- sub_rules_adc.py
+++ sub_rules_adc.py
@@ -4,7 +4,7 @@ def insured_cannot_selfpay(rule_state, rx, messages, error_type, error_rule):
 
         for bag in rx.get("Data", {}).get("eff_order", []):
             for order in bag.get("order", []):
-                if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N":
+                if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N" and order.get("CODE", "") != "OMIF":
                     if bag.get("PATNAME") == "False" and order.get("CTYPE", "") == "自費":
                         messages.append(
                             f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
@@ -21,12 +21,13 @@ def foreign_control_drug_limit(rule_state, rx, messages, error_type, error_rule)
     if rule_state.get(rule) == 'true':
         for bag in rx.get("Data", {}).get("eff_order", []):
             if bag.get("PATNAME") == "True":
+                non_omif_orders = [order for order in bag.get("order", []) if order.get("CODE", "") != "OMIF"]
                 control_drug_count = len(bag.get("order", []))
                 max_control_types = 2 if bag.get("SECTNO") == "精神科" else 1
 
                 if control_drug_count > max_control_types and not bag.get("_warned_control_type_limit", False):
                     detail_lines = []
-                    for each_order in bag.get("order", []):
+                    for each_order in non_omif_orders:
                         detail_lines.append(
                             f"{each_order.get('DIANAME') or each_order.get('NAME')}，頻次：{each_order.get('FREQ', '')}，"
                             f"每次{float(each_order.get('SD', 0))}粒，總量：{each_order.get('TXN_QTY', '')}粒，天數 {int(each_order.get('DAYS', 0))} 天"
@@ -50,7 +51,7 @@ def foreign_max_days(rule_state, rx, messages, error_type, error_rule):
             if bag.get("PATNAME") == "True":
                 max_days = 30 if bag.get("SECTNO") == "精神科" else 7
                 for order in bag.get("order", []):
-                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N":
+                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N" and order.get("CODE", "") != "OMIF":
                         if int(order.get("DAYS", 0)) > max_days:
                             messages.append(
                                 f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
@@ -68,7 +69,7 @@ def foreign_max_dosage(rule_state, rx, messages, error_type, error_rule):
         for bag in rx.get("Data", {}).get("eff_order", []):
             if bag.get("PATNAME") == "True":
                 for order in bag.get("order", []):
-                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N":
+                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N" and order.get("CODE", "") != "OMIF":
                         if float(order.get("SD", 0)) > 1:
                             messages.append(
                                 f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
@@ -86,7 +87,7 @@ def doctor_cannot_prescribe_self(rule_state, rx, messages, error_type, error_rul
         for bag in rx.get("Data", {}).get("eff_order", []):
             if bag.get("DOC") == "True":
                 for order in bag.get("order", []):
-                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N":
+                    if "內用" in order.get("TYPE", "") and order.get("DRUGKIND") != "N" and order.get("CODE", "") != "OMIF":
                         messages.append(
                             f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
                             f"每次{float(order.get('SD', 0))}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
--- sub_rules_oral.py
+++ sub_rules_oral.py
@@ -3,7 +3,7 @@ def oral_drug_checker(rule_state, rx, messages, error_type, error_rule):
     if rule_state.get(rule) == 'true':
         for bag in rx.get("Data", {}).get("eff_order", []):
             for order in bag.get("order", []):
-                if int(order.get('DAYS', 0)) == 1 and "內用" in order.get("TYPE", ""):
+                if int(order.get('DAYS', 0)) == 1 and "內用" in order.get("TYPE", "") and order.get("CODE", "") != "OMIF":
                     messages.append(
                         f"{order.get('DIANAME') or order.get('NAME')}，頻次：{order.get('FREQ', '')}，"
                         f"每次{float(order.get('SD', 0))}粒，總量：{order.get('TXN_QTY', '')}粒，天數 {int(order.get('DAYS', 0))} 天。"
