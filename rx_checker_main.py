69698533f85cf9951aee808cf23f53f5df96f88d
wileyliao <wileyliao9552@gmail.com>
2025-05-21 11:04:00 +0800

    20250521 move block of oral check

--- rx_checker_main.py
+++ rx_checker_main.py
@@ -13,6 +13,9 @@ def main(rx, rule_state):
             if order.get("CODE", "") == "OMIF":
                 messages, error_type = omif_checker(order, messages, error_type, )
 
+                # 口服藥品審查區間
+            messages, error_type, error_rule = oral_drug_checker(order, messages, error_type, error_rule)
+
             # 管制藥品審查區間
             if "內用" not in order.get("TYPE", "") or order.get("DRUGKIND") == "N":
                 return messages, error_type, error_rule
@@ -22,7 +25,5 @@ def main(rx, rule_state):
             messages, error_type, error_rule = foreign_max_dosage(order, bag, messages, error_type, error_rule)
             messages, error_type, error_rule = doctor_cannot_prescribe_self(order, bag, messages, error_type, error_rule)
 
-            # 口服藥品審查區間
-            messages, error_type, error_rule = oral_drug_checker(order, messages, error_type, error_rule)
 
     return messages, error_type, error_rule
